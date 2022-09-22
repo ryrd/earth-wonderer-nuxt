52bdac6d6255da97b6873da8b245e5ada54494c9
ryrd <rdridhani@gmail.com>
2022-09-22 19:01:27 +0800

    toggle anim bug fixed

--- components/parts/Toggle.vue
+++ components/parts/Toggle.vue
@@ -4,6 +4,8 @@ import gsap from 'gsap';
 
 const mode = useModeStore();
 
+const toggleRef = ref();
+
 const toggleDark = () => {
     if (process.client) {
         const tryIt = document.querySelector('#tryit') as HTMLDivElement;
@@ -15,44 +17,47 @@ const toggleDark = () => {
     }
 }
 
+const toggleAnim = gsap.timeline()
+
 onMounted(() => {
     if(localStorage.getItem('toggleClicked') && process.client) {
         const tryIt = document.querySelector('#tryit') as HTMLDivElement;
         tryIt.style.display = 'none';
     };
 
-    // gsap.from('#toggle', {
-    //     duration: 1.3,
-    //     delay: .3,
-    //     y: -100,
-    //     ease: "power4.out"
-    // })
-});
-
-const toggleEnter = el => {
-    console.log(el)
-    gsap.from(el, {
+    toggleAnim.from(toggleRef.value, {
         duration: 1.3,
         delay: .3,
         y: -100,
         ease: "power4.out"
     })
-}
+});
+
+// const toggleEnter = el => {
+//     console.log(el)
+//     gsap.from(el, {
+//         duration: 1.3,
+//         delay: .3,
+//         y: -100,
+//         ease: "power4.out"
+//     })
+// }
 
 
 </script>
 
 <template>
     <div>
-        <transition appear @enter="toggleEnter">
-            <!-- id="toggle" -->
-            <button class='bg-black hover:bg-neutral-900 cursor-point bg-opacity-90 w-auto h-12 md:h-16 absolute top-0 right-0 flex justify-center items-center text-white font-oswald font-light tracking-wide text-base md:text-[1.4vw] px-[3vw] md:px-[1.5vw] landscape:lg:py-[3vh] gap-3 z-50'
+        <!-- <transition appear @enter="toggleEnter"> -->
+            <button class='bg-black hover:bg-neutral-900 cursor-point w-auto h-12 md:h-16 absolute top-0 right-0 flex justify-center items-center text-white font-oswald font-light tracking-wide text-base md:text-[1.4vw] px-[3vw] md:px-[1.5vw] landscape:lg:py-[3vh] gap-3 z-50'
                     @click="toggleDark"
+                    id="toggle"
+                    ref="toggleRef"
                     >
                 {{!mode.darkMode ? 'dark' : 'light'}} mode
                 <img :src="!mode.darkMode ? '../../assets/moon.svg' : '../../assets/sun.svg'" alt="dark mode toggle" class='h-5 md:h-[2vw]' />
             </button>
-        </transition>
+        <!-- </transition> -->
 
         <div class='try-it w-[25vw] md:w-[10vw] h-16 absolute top-14 md:top-20 right-0 flex flex-col justify-center items-center cursor-point font-oswald font-light text-[#d9d9d9] text-sm md:text-base z-50'
              id="tryit">
