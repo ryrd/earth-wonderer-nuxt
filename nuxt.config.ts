3a3dd64681c14338148890aad2fb686fff06aae2
ryrd <rdridhani@gmail.com>
2022-09-27 10:18:52 +0800

    fix loc logo, nuxt config static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     css: ["~/assets/css/style.css"],
+    target: 'static',
     build: {
         postcss: {
             postcssOptions: require("./postcss.config.js"),
